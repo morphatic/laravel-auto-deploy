1840c4944b6aaf29fa4733cdc7219c7369c40673
Morgan Benton <registered@morphatic.com>
2015-11-26 01:43:17 -0500

    published config

--- src/AutoDeployServiceProvider.php
+++ src/AutoDeployServiceProvider.php
@@ -19,8 +19,17 @@ class AutoDeployServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->registerConfig();
         $this->registerDeployInitCommand();
         $this->registerRoutes();
+    }
+
+    private function registerConfig()
+    {
+        $config = realpath(__DIR__.'/../config/config.php');
+        $this->publishes([
+            $config => config_path('auto-deploy.php'),
+        ]);
     }
 
     private function registerDeployInitCommand()
@@ -35,7 +44,7 @@ class AutoDeployServiceProvider extends ServiceProvider
     private function registerRoutes()
     {
         // only register routes if the secret route has been set
-        if (!empty($this->app->config('auto-deploy.route'))) {
+        if (!empty(config('auto-deploy.route'))) {
             include_once __DIR__.'routes.php';
         } else {
             // throw an exception? display a warning?
